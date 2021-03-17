c39875b720f3c176fc3a83a6c4663b8ca3ff5c2e
JosueXA <josue.xicotencal.a@gmail.com>
2021-03-16 20:03:43 -0600

    visual content change with data

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { HeroeList } from '../heroes/HeroeList';
 
 export const MarvelScreen = () => {
+
   return (
     <div>
       <h1>Marvel Screen</h1>
+      <hr />
+
+      <HeroeList publisher="Marvel Comics" />
+
     </div>
   )
 }
